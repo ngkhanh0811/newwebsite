e9edd24b9cc3974766b0f6f620d4888ecb2f57b7
nguyenkhanh08112003 <nguyenkhanh08112003@gmail.com>
2021-11-17 11:32:15 +0700

    baitap17-11

---
+++ js_lesson_03/string-object.js
@@ -0,0 +1,12 @@
+var saying = 'Home sweet home';
+var msg = '<h2>length</h2><p>' + saying.length + '</p>';
+msg += '<h2>uppercase</h2><p>' + saying.toUpperCase()+ '</p>';
+msg += '<h2>lowercase</h2><p>' + saying.toLowerCase()+ '</p>';
+msg += '<h2>character index: 12</h2><p>' + saying.charAt()+ '</p>';
+msg += '<h2>first ee</h2><p>' + saying.indexOf()+ '</p>';
+msg += '<h2>last e</h2><p>' + saying.lastIndexOf()+ '</p>';
+msg += '<h2>character index: 8-14</h2><p>' + saying.substring()+ '</p>';
+msg += '<h2>replace</h2><p>' + saying.replace()+ '</p>';
+
+var el = document.getElementById('info');
+el.innerHTML = msg;
